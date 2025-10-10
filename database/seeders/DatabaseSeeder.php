705f9171ffe318f79d67702130605283ef0a0605
Andre Diofanu <andrediofanu@gmail.com>
2025-10-10 20:26:55 +0700

    Update users, add roles, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,36 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Role::create([
+            'id' => 1,
+            'name' => 'Superadmin',
+        ]);        
+        Role::create([
+            'id' => 2,
+            'name' => 'Guru',
         ]);
+        Role::create([
+            'id' => 3,
+            'name' => 'Siswa',
+        ]);
+        User::factory()->create([
+            'name' => 'Super Admin 1',
+            'email' => 'superadmin1@gmail.com',
+            'password' => bcrypt('123123123'),
+            'role_id' => 1,
+        ]);
+        User::factory()->create([
+            'name' => 'Admin 1',
+            'email' => 'admin1@gmail.com',
+            'password' => bcrypt('123123123'),
+            'role_id' => 2,
+        ]);
+        User::factory()->create([
+            'name' => 'Guru 1',
+            'email' => 'guru1@gmail.com',
+            'password' => bcrypt('123123123'),
+            'role_id' => 3,
+        ]);
+       
     }
 }
