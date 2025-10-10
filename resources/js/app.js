4cbb402a48686f2c8bc4f2b2623bf0b06a096aec
Andre Diofanu <andrediofanu@gmail.com>
2025-10-10 15:06:59 +0700

    Update Dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,17 @@
 import './bootstrap';
+// Core JS
+import './core/bootstrap.bundle.min';
+import './core/popper.min';
+
+// Plugins
+import './plugins/perfect-scrollbar.min';
+import './plugins/smooth-scrollbar.min';
+import './plugins/chartjs.min';
+import './plugins/bootstrap-notify';
+
+// Dashboard main JS
+import './soft-ui-dashboard.min';
+
 
 import Alpine from 'alpinejs';
 
