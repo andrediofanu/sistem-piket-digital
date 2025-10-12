8932c13c3f0066ed02d67ebc50708cddc247e9e9
Andre Diofanu <andrediofanu@gmail.com>
2025-10-12 20:00:31 +0700

    A bit route controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+    {
+        $role = auth()->user()->role->name;
+        // dd($role);
+        if (auth()->user()->isAdminPiket == 1){
+            $siswa = User::where('role_id', 3)->get();
+            // dd($siswa);
+        }
+        return view('admin.index');
+    }
+}
